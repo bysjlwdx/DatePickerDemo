c37bd493d439fa414010ba051b848859c341466f
luo609085431 <609085431@qq.com>
2017-10-20 21:18:08 +0800

    Initial commit

---
+++ app/src/main/java/com/bysjlwdx/datepickerdemo/MainActivity.java
@@ -0,0 +1,56 @@
+package com.bysjlwdx.datepickerdemo;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+public class MainActivity extends Activity implements OnClickListener{
+    private TextView tvShowDialog;
+    private Calendar cal;
+    private int year,month,day;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取当前日期
+        getDate();
+
+        tvShowDialog=(TextView) findViewById(R.id.tvShowDialog);
+        tvShowDialog.setOnClickListener(this);
+    }
+    //获取当前日期
+    private void getDate() {
+        cal=Calendar.getInstance();
+        year=cal.get(Calendar.YEAR);       //获取年月日时分秒
+        Log.i("wxy","year"+year);
+        month=cal.get(Calendar.MONTH);   //获取到的月份是从0开始计数
+        day=cal.get(Calendar.DAY_OF_MONTH);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tvShowDialog:
+                OnDateSetListener listener=new OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker arg0, int year, int month, int day) {
+                        tvShowDialog.setText(year+"-"+(++month)+"-"+day);      //将选择的日期显示到TextView中,因为之前获取month直接使用，所以不需要+1，这个地方需要显示，所以+1
+                    }
+                };
+                DatePickerDialog dialog=new DatePickerDialog(MainActivity.this, 0,listener,year,month,day);//后边三个参数为显示dialog时默认的日期，月份从0开始，0-11对应1-12个月
+                dialog.show();
+                break;
+
+            default:
+                break;
+        }
+    }
+}
